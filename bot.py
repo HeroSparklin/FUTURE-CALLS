116f14bea2619da528225a4fb9b1771b746a2ed7
HeroSparklin <122805594+HeroSparklin@users.noreply.github.com>
2026-09-24 14:35:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-import requests, os
+import os, requests
 TOKEN = os.getenv("BOT_TOKEN")
-CHAT = os.getenv("CHAT_ID")
-print(f"CHAT={CHAT} TOKEN starts with {TOKEN[:10]}")
+CHAT_ID = os.getenv("CHAT_ID")
+print(f"BOT_TOKEN exists: {TOKEN is not None}")
+print(f"CHAT_ID: {CHAT_ID}")
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-r = requests.post(url, json={"chat_id": CHAT, "text": "TEST HELLO"})
-print("TELEGRAM REPLY:")
+r = requests.post(url, json={"chat_id": CHAT_ID, "text": "✅ SUCCESS! Herocallss bot is NOW CONNECTED!"})
 print(r.text)
